97dd37f403ad8de92b9e89b106ec63384edc3e37
suren.islyaev@gmail.com <suren.islyaev@gmail.com>
2015-04-22 12:09:46 +0100

    Hi

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExcelDna.Integration;
+
+namespace MyLibrary
+{
+    public class Class1
+    {
+        [ExcelFunction(Description = "My first Excel-DNA function")]
+        public static string MyFirstFunction(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
